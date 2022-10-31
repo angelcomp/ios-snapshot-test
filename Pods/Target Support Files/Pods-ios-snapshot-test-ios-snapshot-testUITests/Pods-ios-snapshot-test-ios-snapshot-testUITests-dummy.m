add91ae20bc1280f4fe0ece90473389ed02252a1
Angelica dos Santos <santosangelicassp@gmail.com>
2022-10-31 16:22:57 -0300

    installing snapshot lib

---
+++ Pods/Target Support Files/Pods-ios-snapshot-test-ios-snapshot-testUITests/Pods-ios-snapshot-test-ios-snapshot-testUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ios_snapshot_test_ios_snapshot_testUITests : NSObject
+@end
+@implementation PodsDummy_Pods_ios_snapshot_test_ios_snapshot_testUITests
+@end
---
+++ Pods/Target Support Files/Pods-ios-snapshot-test/Pods-ios-snapshot-test-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ios_snapshot_test : NSObject
+@end
+@implementation PodsDummy_Pods_ios_snapshot_test
+@end
