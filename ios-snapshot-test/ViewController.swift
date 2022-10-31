cc31a5fd8113c0126bd49d55f14fc6785969d810
Angelica dos Santos <santosangelicassp@gmail.com>
2022-10-31 16:09:57 -0300

    Initial Commit

---
+++ ios-snapshot-test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ios-snapshot-test
+//
+//  Created by Angelica dos Santos on 31/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
